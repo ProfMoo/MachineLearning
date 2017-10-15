4b7f74fd404cddae1efbf8b98cbdaa95f23442dc
Shane Moo <obries6@rpi.edu>
2017-10-14 21:04:30 -0400

    3.1 complete

--- HW06/prob3-1.py
+++ HW06/prob3-1.py
@@ -1,6 +1,7 @@
 import numpy
 import scipy
 import matplotlib
+import copy
 
 import matplotlib.pyplot as plt
 from random import randint
@@ -12,8 +13,8 @@ def formula(x):
 	print("w: ", w)
 	return ((-w[1]*x)/w[2])-(w[0]/w[2])
 
-def formula2(w2, x):
-	return 
+def formula2(x):
+	return (x*wlinslope + wlinyint)
 
 def cart2pol(x, y):
 	rho = numpy.sqrt(x**2 + y**2)
@@ -25,7 +26,7 @@ def pol2cart(rho, theta):
 	y = rho * numpy.sin(theta)
 	return[x, y]
 
-def plot(data, h):
+def plot(data, h, type):
 	i = 0
 	rx = []
 	ry = []
@@ -36,23 +37,35 @@ def plot(data, h):
 
 	while (i < len(data)):
 		if (h[i] == -1):
-			bx.append(data[i][1])
-			by.append(data[i][2])
+			bx.append(data[i][0+type])
+			by.append(data[i][1+type])
 		elif (h[i] == 1):
-			rx.append(data[i][1])
-			ry.append(data[i][2])
+			rx.append(data[i][0+type])
+			ry.append(data[i][1+type])
 		i += 1	
 
-	lx = numpy.linspace(0,upperBound,2000)
+	if (type == 0):
+		lx = numpy.linspace(0,upperBound,2000)
+
+		gx = numpy.array(range(0, upperBound))
+		gy = formula(gx)
+		plt.plot(gx, gy)
+
+		plt.plot(rx, ry, 'ro')
+		plt.plot(bx, by, 'bx')
+		plt.axis([0,upperBound,0,upperBound])
+		plt.show()
+
+def plotwlin(wlin):
+	wlinslope = -1.0*wlin[1][0]/wlin[2][0]
+	wlinyint = -1.0*wlin[0][0]/wlin[2][0]
+
+	print("wlinslope: ", wlinslope)
+	print("wlinyint: ", wlinyint)
 
 	gx = numpy.array(range(0, upperBound))
-	gy = formula(gx)
+	gy = formula2(gx)
 	plt.plot(gx, gy)
-
-	plt.plot(rx, ry, 'ro')
-	plt.plot(bx, by, 'bx')
-	plt.axis([0,upperBound,0,upperBound])
-	plt.show()
 
 def checkBad(k, data, h):
 	insideSign = w[0]*1+w[1]*data[k][0]+w[2]*data[k][1]
@@ -84,7 +97,6 @@ def makeDataCircles(data):
 			theta = random.uniform(0,180)
 			rho = random.uniform(10,15)
 			location = pol2cart(rho, theta)
-			print("location: ", location)
 			location[0] += 17.5
 			if (location[1] < 0):
 				location[1] = -(location[1])
@@ -110,13 +122,13 @@ def makeDataCircles(data):
 		i += 1
 	return h
 
-def fixData(data2):
+def fixData(data):
+	data2 = copy.deepcopy(data)
 	i = 0
 	while (i < len(data2)):
-		data[i].insert(0, 1)
+		data2[i].insert(0, 1)
 		i += 1
 
-	print("data2: ", data2)
 	return data2
 
 def runSimulation(h ,data):
@@ -146,31 +158,38 @@ if __name__ == "__main__":
 	x2 = []
 
 	data = []
+	datacircles = []
 	red = []
 	blue = []
 	h = []
+	h2 = []
 
-	numPoints = 10
+	numPoints = 2000
 	upperBound = 50
 
-	h = makeDataCircles(data)
+	h = makeDataCircles(datacircles)
 	#print("data:", data)
 	#print("h: ", h2)
 
-	runSimulation(h, data)
+	runSimulation(h, datacircles)
 	#print("data:", data)
 
 	#regression: 3.1b
-	data2 = []
-	data2 = fixData(data)
-	x_matrix = numpy.matrix(data2)
+	datacircles2 = fixData(datacircles)
+	x_matrix = numpy.matrix(datacircles2)
 
 	#h_fix = fixh(h)
 	y_matrix = numpy.matrix(h)
-	print("x_matrix: ", x_matrix)
-	print("y_matrix: ", y_matrix)
+	#print("x_matrix: ", x_matrix)
+	#print("y_matrix: ", y_matrix)
 	wlin = (numpy.linalg.inv(numpy.transpose(x_matrix) * x_matrix) * numpy.transpose(x_matrix)) * numpy.transpose(y_matrix)
 	print("wlin: ", wlin)
+	wlinslope = -1.0*wlin[1][0]/wlin[2][0]
+	wlinyint = -1.0*wlin[0][0]/wlin[2][0]
+	wlinslope = wlinslope.item(0,0)
+	wlinyint = wlinyint.item(0,0)
 	#w2 = 
 
-	plot(data, h)
+	plotwlin(wlin)
+	plot(datacircles, h, 0)
+	#plot(data2, h, 1)
