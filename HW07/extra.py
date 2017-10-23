1a1eed7bfb533d6ae2573ed6d261d5c870b0f66c
Shane Moo <obries6@rpi.edu>
2017-10-22 20:32:38 -0400

    Working on training data

--- HW07/extra.py
+++ HW07/extra.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from random import randint
 import random
 
+w = [-1,1,1]
+
 def getIntensity(trainingDigit):
 	intensity = 0
 	i = 0
@@ -37,15 +39,26 @@ def getSymmetry(trainingDigit):
 		i += 1
 	return (xsymmetry+ysymmetry)
 
-def getData(xs, ys):
+def getData(x1, y1, x5, y5, one):
 	databack = []
 	singledata = []
 	i = 0
-	while (i < len(xs)):
+	while (i < len(x1)):
 		singledata = []
-		singledata.append(1)
-		singledata.append(xs[i])
-		singledata.append(ys[i])
+		if (one == 1):
+			singledata.append(1)
+		singledata.append(x1[i])
+		singledata.append(y1[i])
+		databack.append(singledata)
+		i += 1
+
+	i = 0
+	while (i < len(x5)):
+		singledata = []
+		if (one == 1):
+			singledata.append(1)
+		singledata.append(x5[i])
+		singledata.append(y5[i])
 		databack.append(singledata)
 		i += 1
 
@@ -64,21 +77,109 @@ def getYs(x1, x5):
 
 	return ys
 
-def plotwlin(wlin):
-	wlinslope = -1.0*wlin[1][0]/wlin[2][0]
-	wlinyint = -1.0*wlin[0][0]/wlin[2][0]
+def formula2(x, wslope, wyint):
+	return (x*wslope + wyint)
 
-	print("wlinslope: ", wlinslope)
-	print("wlinyint: ", wlinyint)
+def plotwlin(wother, which):
 
-	gx = numpy.array(range(0, upperBound))
-	gy = formula2(gx)
-	plt.plot(gx, gy)
+	print("wother:", wother)
 
+	if (which == 0):
+		wslope = -1.0*wother[1][0]/wother[2][0]
+		wyint = -1.0*wother[0][0]/wother[2][0]
+		wslope = wslope.item(0,0)
+		wyint = wyint.item(0,0)
+		print("wslope: ", wslope)
+		print("wyint: ", wyint)
+		gx = numpy.array(range(97, 108))
+		gy = formula2(gx, wslope, wyint)
+		plt.plot(gx, gy, 'r')
+	if (which == 1):
+		wslope = -1.0*wother[1]/wother[2]
+		wyint = -1.0*wother[0]/wother[2]
+		print("wslope: ", wslope)
+		print("wyint: ", wyint)
+		gx = numpy.array(range(95, 108))
+		gy = formula2(gx, wslope, wyint)
+		plt.plot(gx, gy, 'b')
+
+def checkBad(k, data, h, wcheck):
+	insideSign = wcheck[0]*1+wcheck[1]*data[k][0]+wcheck[2]*data[k][1]
+	if (insideSign > 0 and h[k] > 0):
+		return True
+	elif (insideSign > 0 and h[k] < 0):
+		return False
+	elif (insideSign < 0 and h[k] < 0):
+		return True
+	elif (insideSign < 0 and h[k] > 0):
+		return False
+
+def pocketCheck(wbest, data, h):
+	i = 0
+	wbestbad = 0
+	wbad = 0
+	while (i < len(data)):
+		if (checkBad(i, data, h, w) == False):
+			wbad += 1
+		if (checkBad(i, data, h, wbest) == False):
+			wbestbad += 1
+		i += 1
+
+	if (wbad < wbestbad):
+		print("wbad: ", wbad)
+		print("wbestbad: ", wbestbad)
+		print("found better, replacing...")
+		print("w: ", w)
+		return True
+	return False
+
+def updateWeights(k, data, h):
+	#print("wpre: ", w)
+	j = 0
+	while (j < 3):
+		if (j == 0):
+			w[j] = w[j] + (h[k])
+		else:
+			w[j] = w[j] + (h[k]*data[k][j-1])
+		j += 1
+
+def runSimulation(h, data):
+	numIter = 0
+	complete = False
+	wbest = w[:]
+	print("wbest(wlin right now): ", wbest)
+	while(complete == False and numIter < pocketTop): ##while iterations still need to be done
+		
+		if (numIter%100 == 0):
+			print("numIter: ", numIter)
+
+		#check for pocket algorithm
+		if(pocketCheck(wbest, data, h)):
+			wbest = w[:]
+
+		i = 0
+		checking = False
+		while (i < len(data)): ##looping through data
+			checking = checkBad(i, data, h, w)
+			if (checking == True):
+				if (i == len(data)-1): ##if you're done
+					complete = True
+					break
+				else:
+					i += 1
+					continue
+			elif (checking == False): ##if there is a wrong value
+				updateWeights(i, data, h)
+				break
+			i += 1
+		numIter += 1
+
+	return wbest
 
 if __name__ == "__main__":
 	f = open("ZipDigits.train", 'r')
 
+	pocketTop = 1000
 	x1 = []
 	y1 = []
 	x5 = []
@@ -97,10 +198,10 @@ if __name__ == "__main__":
 
 	#make data with 1 in beginning
 	data = []
-	data.append(getData(x1, y1))
-	data.append(getData(x5, y5))
-	print("data: ", data)
-
+	data2 = []
+	data = getData(x1, y1, x5, y5, 1)
+	data2 = getData(x1, y1, x5, y5, 0)
+	#print("data: ", data)
 	ys = getYs(x1, x5)
 
 	#get wlin
@@ -108,18 +209,23 @@ if __name__ == "__main__":
 	y_matrix = numpy.matrix(ys)
 	print("x_matrix: ", x_matrix)
 	print("y_matrix: ", y_matrix)
-	wlin = (numpy.linalg.inv(numpy.transpose(x_matrix) * x_matrix) * numpy.transpose(x_matrix)) * numpy.transpose(y_matrix)
+	x_matrix_trans = numpy.transpose(x_matrix)
+	wlin = (numpy.linalg.inv(x_matrix_trans * x_matrix) * x_matrix_trans) * numpy.transpose(y_matrix)
 	print("wlin: ", wlin)
-	wlinslope = -1.0*wlin[1][0]/wlin[2][0]
-	wlinyint = -1.0*wlin[0][0]/wlin[2][0]
-	wlinslope = wlinslope.item(0,0)
-	wlinyint = wlinyint.item(0,0)
-	plotwlin(wlin)
+
+	w[0] = wlin[0].item(0,0)
+	w[1] = wlin[1].item(0,0)
+	w[2] = wlin[2].item(0,0)
+
+	wbest = runSimulation(ys, data2)
+	print("wbest: ", wbest)
 
 	#now, plot
 	fig = plt.figure()
 	plt.plot(x5, y5, 'rx')
 	plt.plot(x1, y1, 'bo')
+	plotwlin(wlin, 0)
+	plotwlin(wbest, 1)
 	fig.suptitle('digits', fontsize = 20)
 	plt.xlabel('asymmetry', fontsize = 18)
 	plt.ylabel('intensity', fontsize = 16)
