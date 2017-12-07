d2d874f4f24ef0211a92051d462a78b26ed62f20
Shane Moo <obries6@rpi.edu>
2017-12-07 00:00:25 -0500

    Started back_prop

--- HW12/prob1a.py
+++ HW12/prob1a.py
@@ -11,26 +11,40 @@ def tanh(x):
 	return x
 	#return np.tanh(x)
 
+def tanhprime(x):
+	return 1
+
 def forward_prop(x1, y, W):
 	i = 0
 	S = list()
 	X = list()
 	X.append(x1)
+	vec_op = np.vectorize(tanh)
 	while (i < len(W)):
 		S.append(np.dot(np.transpose(W[i]), X[i]))
-		X.append(np.transpose(np.matrix(np.append([1], S[i]))))
+		vec_matrix = vec_op(S[i])
+		X.append(np.transpose(np.matrix(np.append([1], vec_matrix))))
 		i += 1
 
 	return(S, X)
 
-def backward_prop():
-	return
+def backward_prop(S, X, W, y):
+	D = list()
+	D.append( 2 * (X[0].item(0,0) - y) * tanhprime(S[len(S)-1]))
+	i = 0
+	while (i < len(W)):
+		#need two loops in here
+
+		#get theta prime here
+		#get sensitivty here and append 
+		i += 1
 
 def gradient_descent(x1, y, W):
 	S, X = forward_prop(x1, y, W)
 	print("S: ", S)
 	print("X: ", X)
 	print("W: ", W)
+	backward_prop(S, X, W, y)
 
 def main():
 	x1 = np.matrix('1;1;1')
